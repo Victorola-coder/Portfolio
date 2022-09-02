7ef687b0f7c353a7986d945fecb22c40050ef1e8
Mgbemele IkeChukwu <mgbemeleikechukwu@gmail.com>
2022-09-02 01:52:48 +0100

    implemented the contact form

--- index.js
+++ index.js
@@ -1,100 +1,112 @@
-const fullName = document.querySelector('.name')
-const email = document.querySelector('.email')
-const text = document.querySelector('.text')
-const button = document.querySelector('.button')
-const error = document.querySelector('.error')
-const form = document.querySelector('form')
+const fullName = document.querySelector('.name');
+const email = document.querySelector('.email');
+const subject = document.querySelector('.subject');
+const body = document.querySelector('.text');
+const button = document.querySelector('.button');
+const error = document.querySelector('.error');
+const success = document.querySelector('.success');
+const form = document.querySelector('form');
 
 const allsections = document.querySelectorAll('.section');
 //allsections.classList.add('section--hidden')
-const revealSection = function(entries, observer) {
+const revealSection = function (entries, observer) {
     const [entry] = entries;
     //console.log(entry)
 
     if (!entry.isIntersecting) return;
     entry.target.classList.remove('section--hidden');
     observer.unobserve(entry.target);
-}
+};
 
 const sectionobserver = new IntersectionObserver(revealSection, {
     root: null,
     threshold: 0.15,
-})
-allsections.forEach(function(section) {
+});
+allsections.forEach(function (section) {
     sectionobserver.observe(section);
-    section.classList.add('section--hidden')
-})
+    section.classList.add('section--hidden');
+});
 
 button.addEventListener('click', (e) => {
-    e.preventDefault()
-    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (fullName.value.length == 0 || email.value.length == 0 || text.value.length == 0) {
-        error.style.display = 'block'
-        error.innerHTML = 'All fields are required!🙄'
+    e.preventDefault();
+    const re =
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (
+        fullName.value.length == 0 ||
+        subject.value.length == 0 ||
+        body.value.length == 0
+    ) {
+        error.style.display = 'block';
+        error.innerHTML = 'All fields are required!🙄';
+        setTimeout(() => {
+            error.style.display = 'none';
+        }, 1500);
+        return;
     }
-    setTimeout(() => {
-        error.style.display = 'none'
-    }, 1500)
 
     if (re.test(String(email.value).toLowerCase()) !== true) {
-        error.innerHTML = "Invalid Email syntax!"
-        error.style.display = 'block'
+        error.innerHTML = 'Invalid Email syntax!';
+        error.style.display = 'block';
         setTimeout(() => {
-            error.innerHTML = ""
-            error.style.display = 'none'
-        }, 1500)
+            error.innerHTML = '';
+            error.style.display = 'none';
+        }, 1500);
     }
 
-    try {
-        $.ajax({
-            async: true,
-            url: "https://portfoliobackendservices.herokuapp.com/sendmail",
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            data: {
-                name: fullName.value,
-                email: email.value,
-                message: text.value,
-            },
-            success: async(data) => {
-                if (data.status == '401') {
-                    console.log(data)
-                    error.innerHTML = 'Try again later'
-                    error.style.display = 'block'
-                    setTimeout(() => {
-                        error.innerHTML = ""
-                        error.style.display = 'none'
-                    }, 1500)
-                } else {
-                    if (data.status == "200") {
-                        // error.innerHTML = 'Your Message has been sent.😊'
-                        error.style.display = 'block'
-                        console.log('Heyy!', data, fullName.value, email.value, text.value)
-                        setTimeout(() => {
-                            error.innerHTML = ""
-                            error.style.display = 'none'
-                            fullName.value = '';
-                            email.value = ''
-                            text.value = ''
-                        }, 1500)
-                    }
-                }
-            }
-        })
-    } catch (error) {
-        alert('Something went wrong, Try again later.')
-    }
-    if (fullName.value && email.value && text.value !== "") {
-        form.reset();
-    }
-})
+    $.ajax({
+        async: true,
+        url: 'https://victorola.herokuapp.com/api/email',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        data: {
+            subject: subject.value,
+            email: email.value,
+            message: body.value,
+            name: fullName.value,
+        },
+        success: async (data) => {
+            // error.innerHTML = 'Your Message has been sent.😊'
+            error.innerHTML = '';
+            error.style.display = 'none';
+            success.style.display = 'block';
 
-var typed = new Typed(".anim", {
-        strings: ["Web Developer", "Software Developer", "Graphic Designer", "Freelancer", "Student"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
+            setTimeout(() => {
+                fullName.value = '';
+                subject.value = '';
+                email.value = '';
+                body.value = '';
+                success.style.display = 'none';
+            }, 4000);
+        },
+        error: (err) => {
+            error.innerHTML = err.responseJSON.message;
+            error.style.display = 'block';
+            setTimeout(() => {
+                fullName.value = '';
+                subject.value = '';
+                email.value = '';
+                body.value = '';
+                error.style.display = 'none';
+            }, 4000);
+        },
     });
 
+    if (fullName.value || email.value || body.value || subject.value !== '') {
+        form.reset();
+    }
+});
+
+var typed = new Typed('.anim', {
+    strings: [
+        'Web Developer',
+        'Software Developer',
+        'Graphic Designer',
+        'Freelancer',
+        'Student',
+    ],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+});
